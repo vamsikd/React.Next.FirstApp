682926b6b41f418f5fc8aa631aeb03be39d94954
Vamsi Krishna D <vamsi.doddapaneni@outlook.com>
2023-08-23 20:56:42 +0530

    added typescript dependencies

--- app/layout.jsx
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({children}){
-    return (
-        <html>
-            <bofy>
-                {children}
-            </bofy>
-        </html>
-    )
-}
---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from "react"
+
+type RootLayoutProps = {
+    children: ReactNode
+}
+
+export default function RootLayout({children}:RootLayoutProps){
+    return (
+        <html>
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}

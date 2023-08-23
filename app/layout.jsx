5bde6d9b6adf173edb3fe7e063d96e93c1e291a2
Vamsi Krishna D <vamsi.doddapaneni@outlook.com>
2023-08-23 20:49:30 +0530

    hellow world from next app

---
+++ app/layout.jsx
@@ -0,0 +1,9 @@
+export default function RootLayout({children}){
+    return (
+        <html>
+            <bofy>
+                {children}
+            </bofy>
+        </html>
+    )
+}
---
+++ app/page.jsx
@@ -0,0 +1,5 @@
+export default function HomePage(){
+    return (
+        <h1>Welcome to Next js App</h1>
+    )
+}
